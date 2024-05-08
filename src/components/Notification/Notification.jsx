282eed60e8a9d0d34f2f694772b8e2bfd1c79542
KiraKostina <kostina.kira@gmail.com>
2024-05-08 19:56:12 +0300

    prepare components

--- src/components/Notification/Notification.jsx
+++
@@ -1,9 +0,0 @@
-import css from "./Notification.module.css"
-export default function Notification() {
-    return (
-        <div>
-            <p className={css.notification_message}>No feedback yet</p>
-        </div>
-    );
-};
-
