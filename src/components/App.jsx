566cb7b4307836101c8a63e1b508be44186c9700
KiraKostina <kostina.kira@gmail.com>
2024-05-08 23:47:41 +0300

    upd

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function App() {
+
+return (
+    <div>
+      <h1>Phonebook</h1>
+      {/* <ContactForm />
+      <SearchBox />
+      <ContactList /> */}
+    </div>
+  )
+}
