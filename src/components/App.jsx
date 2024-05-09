d8ddcd958863349a52b105abb08d5ac0f87c54f8
KiraKostina <kostina.kira@gmail.com>
2024-05-09 19:20:44 +0300

    add searchbox

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import { useState } from 'react';
 import initialContacts from '../contacts.json';
 import ContactList from './ContactList/ContactList';
+import css from './App.module.css';
+import SearchBox from './SearchBox/SearchBox';
 
 
 export default function App() {
   const [contacts, setContacts] = useState(initialContacts);
+  const [search, setSearch] = useState('');
+
+  const visibleContacts = contacts.filter((contact) => contact.name.toLowerCase().includes(search.toLowerCase()));
 
   return (
-    <div>
+    <div className={css.container}> 
       <h1>Phonebook</h1>
-      {/* <ContactForm />
-      <SearchBox /> */}
-      <ContactList contacts={contacts} />
+      {/* <ContactForm /> */}
+      <SearchBox value={search} onSearch={setSearch} />
+      <ContactList contacts={visibleContacts} />
     </div>
   );
 }
